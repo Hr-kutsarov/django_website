79d68b4ae9d993c68a6f77dd4ec8176d104de18a
Hristo Kutsarov <103206944+Hr-kutsarov@users.noreply.github.com>
2022-12-09 20:07:32 +0200

    created models
    Created 'Artist', 'Play', 'Ticket', 'User'

--- core/models.py
+++ core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    first_name = models.CharField('First Name', max_length=60, blank=False, null=False)
+    last_name = models.CharField('Last Name', max_length=60, blank=False, null=False)
+    photo = models.ImageField('Photo')
+    bio = models.CharField('Bio', max_length=360, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Play(models.Model):
+    title = models.CharField('Title', max_length=60, blank=False, null=False)
+    genre = models.CharField('Genre', max_length=20, blank=False, null=False)
+    date = models.DateField('Date', blank=False, null=False)
+    time = models.TimeField('Time', blank=False, null=False)
+    description = models.CharField('Description', max_length=360, blank=True, null=True)
+    artists = models.ForeignKey(Artist, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Ticket(models.Model):
+    seat_number = models.IntegerField('Seat Number')
+    price = models.DecimalField('Price', max_digits=5, decimal_places=2)
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    username = models.CharField('username', max_length=30, blank=False, null=False)
+    password = models.CharField('password', max_length=30, blank=False, null=False)
+    email = models.EmailField('E-mail', blank=False, null=False)
+    first_name = models.CharField('First Name', max_length=60, blank=False, null=False)
+    last_name = models.CharField('Last Name', max_length=60, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
--- core/views.py
+++ core/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
+from django.views.generic import TemplateView, FormView, ListView
 
 
 def home(request):
     date = datetime.now()
 
     return render(request, 'plays/home.html', {'date': date})
+
